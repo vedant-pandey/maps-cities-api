097c8e3703ab06204c6c7edb188dda12a55d06bb
Vedant Pandey <vedantpandey46@gmail.com>
2020-01-07 00:55:14 +0530

    Integrate cities API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,65 @@
-import { Component } from '@angular/core';
+import { 
+          Component, 
+          OnInit 
+       }              from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-enum DEFAULT {
-  STATE = 'Select a state',
-  CITY  = 'Select a city'
+interface DistrictInfo {
+  City     : string
+  State    : string
+  District : string
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector    : 'app-root',
+  templateUrl : './app.component.html',
+  styleUrls   : ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  selectedState: string  = DEFAULT.STATE
-  selectedCity:  string  = DEFAULT.CITY
-  stateSelected: boolean = false
+  selectedState : string  = 'Select a state'
+  selectedCity  : string  = 'Select a city'
+  dataLoaded    : boolean = false
 
-  states: string[] = [
-    'Arunachal Pradesh',
-    'Karnataka',
-    'Maharashtra',
-    'Madhya Pradesh',
-    'Uttar Pradesh',
-  ]
-  cities: string[] = [
-    'Delhi',
-    'Lucknow',
-    'Mumbai',
-    'Bangalore',
-  ]
+  districtInfos : DistrictInfo[]
+  states        : string[]
+  cities        : string[]
 
-  changeSelectedState(state: string) {
-    this.stateSelected = true
-    this.selectedState = state
+  constructor(private http : HttpClient) {}
+
+  async ngOnInit() {
+    
+    const resp = this.http.get('https://indian-cities-api-nocbegfhqg.now.sh/cities')
+
+    resp.subscribe((districtInfos: DistrictInfo[]) => {
+      this.dataLoaded    = true
+      this.districtInfos = districtInfos
+      this.states        = [
+        ...new Set(
+          this.districtInfos
+            .map(districtInfo => districtInfo.State)
+            .sort()
+        )
+      ]
+    })
+    
   }
 
-  changeSelectedCity(city: string) {
+  changeSelectedState(state : string) {
+    this.selectedState = state
+    if (this.dataLoaded) {
+      this.cities = [
+        ...new Set(
+          this.districtInfos
+            .filter(districtInfo => districtInfo.State === state)
+            .map(districtInfo => districtInfo.City)
+        )
+      ]
+    }
+  }
+
+  changeSelectedCity(city : string) {
     this.selectedCity = city
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgbModule }      from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule }  from '@angular/platform-browser';
 import { NgModule }       from '@angular/core';
 import { AppComponent }   from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { AppComponent }   from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
