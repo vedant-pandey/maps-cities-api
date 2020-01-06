428d6a3a57f91cffe1aed55af005c92ac7bf7e47
Vedant Pandey <vedantpandey46@gmail.com>
2020-01-06 23:55:02 +0530

    Add bootstrap and build UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+enum DEFAULT {
+  STATE = 'Select a state',
+  CITY  = 'Select a city'
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'maps-cities-api';
+
+  selectedState: string  = DEFAULT.STATE
+  selectedCity:  string  = DEFAULT.CITY
+  stateSelected: boolean = false
+
+  states: string[] = [
+    'Arunachal Pradesh',
+    'Karnataka',
+    'Maharashtra',
+    'Madhya Pradesh',
+    'Uttar Pradesh',
+  ]
+  cities: string[] = [
+    'Delhi',
+    'Lucknow',
+    'Mumbai',
+    'Bangalore',
+  ]
+
+  changeSelectedState(state: string) {
+    this.stateSelected = true
+    this.selectedState = state
+  }
+
+  changeSelectedCity(city: string) {
+    this.selectedCity = city
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { NgbModule }      from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule }  from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { AppComponent }   from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
